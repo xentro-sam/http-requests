963a91779edb25c19663fe7240f3376a53b49ef7
Shivam Agarwal <itisshivam22@gmail.com>
2023-01-24 22:32:20 +0530

    feat: create a simple get route using vanilla NodeJS

---
+++ getRoute.js
@@ -0,0 +1,18 @@
+const http = require('http')
+const PORT = 3000
+
+http.createServer((req, res) => {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+
+    let url = req.url
+
+    console.log('Hello world')
+
+    if(url === '/') {
+        res.write('Hello world')
+    }
+    res.end()
+})
+.listen(PORT, () => {
+    console.log("Server started at PORT - ", PORT)
+})
