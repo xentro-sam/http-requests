d7f070ffa698ed3536d6179011c87a3dc9529a6c
Shivam Agarwal <itisshivam22@gmail.com>
2023-01-25 10:44:13 +0530

    feat: create GET route to get a single task

--- todoRoutes.js
+++ todoRoutes.js
@@ -13,6 +13,19 @@ http.createServer(async (req, res) => {
       res.writeHead(200, {'Content-Type': 'application/json'});
       res.end(JSON.stringify(tasks));
     }
+    if(url.match(/tasks\/([0-9]+)/)) {
+      try {
+        const id = url.split('/')[2];
+        const task = await Todo.getTask(id);
+        console.log(task);
+        res.writeHead(200, {'Content-Type': 'application/json'});
+        res.end(JSON.stringify(task));
+      }
+      catch(error) {
+        res.writeHead(200, {'Content-Type': 'application/json'});
+        res.end(JSON.stringify({message: error}));
+      }
+    }
   }
 })
   .listen(PORT, () => {
